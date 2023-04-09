d9dbf429f5d87777cae088915be2bdf9f9da919b
omid ramezani <116552870+omidreact@users.noreply.github.com>
2023-04-09 18:56:53 +0330

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+
+import usePanResponder from './usePanResponder'
+
+export default function App() {
+  const [state, panHandlers] = usePanResponder()
+
+  const { dragging, initialY, initialX, offsetY, offsetX } = state
+
+  const style = {
+    backgroundColor: dragging ? '#2DC' : '#0BA',
+    transform: [
+      { translateX: initialX + offsetX },
+      { translateY: initialY + offsetY },
+    ],
+  }
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={()=>alert("کلیک")}>
+      <View
+        // Put all panHandlers into the View's props
+        {...panHandlers}
+        style={[styles.square, style]}
+      >
+        <Text style={styles.text}>کلیک کنید</Text>
+      </View>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  square: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+})
